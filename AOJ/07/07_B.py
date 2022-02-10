8556bd7b3e594ae33c2c4298f13f7e2724f236ab
unknown <ryu185132@gmail.com>
2022-02-10 09:24:57 +0900

    topic-7 fixing coding rules

--- AOJ/07/07_B.py
+++ AOJ/07/07_B.py
@@ -1,15 +1,17 @@
 from itertools import combinations
 
-def solve(n,x):
-    C = list(combinations(range(1,n+1),3))
+
+def solve(n, x):
+    C = list(combinations(range(1, n+1), 3))
     cnt = 0
     for c_ in C:
         if sum(c_) == x:
             cnt += 1
     print(cnt)
 
+
 while True:
-    n,x = map(int, input().split())
+    n, x = map(int, input().split())
     if n == 0 and x == 0:
         break
-    solve(n,x)
+    solve(n, x)
