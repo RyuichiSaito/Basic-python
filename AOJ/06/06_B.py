86047795836c5322852d73d13ccc1a471173e8d0
unknown <ryu185132@gmail.com>
2022-02-10 09:28:22 +0900

    topic-6 fixing coding rules

--- AOJ/06/06_B.py
+++ AOJ/06/06_B.py
@@ -1,5 +1,5 @@
 tranp = [[] for _ in range(4)]
-idxs = ['S','H','C','D']
+idxs = ['S', 'H', 'C', 'D']
 for _ in range(int(input())):
     a, x = map(str, input().split())
     idx = idxs.index(a)
@@ -7,7 +7,6 @@ for _ in range(int(input())):
 
 for i in range(4):
     tranp[i].sort()
-    for j in range(1,14):
+    for j in range(1, 14):
         if j not in tranp[i]:
             print('{} {}'.format(idxs[i], j))
-   
--- AOJ/06/06_C.py
+++ AOJ/06/06_C.py
@@ -1,6 +1,6 @@
 house = [[[0 for _ in range(10)] for _ in range(3)] for _ in range(4)]
 for _ in range(int(input())):
-    t, x, y, n = map(lambda x:int(x)-1, input().split())
+    t, x, y, n = map(lambda x: int(x)-1, input().split())
     house[t][x][y] += (n+1)
 
 for i in range(4):
